5f6546becc0b17e1a4f229b0bd14697ab80d15d8
Adam Berg <adam@xyzdigital.com>
2022-05-04 18:33:47 -0700

    Add basic analogRead and sd card writing loop
    Seeing 8192 bytes per second

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,44 @@
 const char filename[] = "demo.txt";
 
 File txtFile;
+char dataBuffer[2048];
+unsigned long startMillis = 0;
+int position = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  while (!Serial);
 
-  if (!SD.begin(D8)) {
+  while (!Serial)
+    ;
+
+  if (!SD.begin(D8))
+  {
     Serial.println("Card failed, or not present");
-    while (1);
+    while (1)
+      ;
   }
 
   txtFile = SD.open(filename, FILE_WRITE);
-  txtFile.println("Hello World!");
-  txtFile.close();
+  startMillis = millis();
 }
 
-void loop() {
+void loop()
+{
+  if (millis() - startMillis >= 1000)
+  {
+    txtFile.close();
+  }
+  else
+  {
+    int sensorValue = analogRead(A0);
+    dataBuffer[position] = 'a';
+    position = position + 1;
 
+    if (position == 2048)
+    {
+      txtFile.write(dataBuffer, 2048);
+      position = 0;
+    }
+  }
 }
